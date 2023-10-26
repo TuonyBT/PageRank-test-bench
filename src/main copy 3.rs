69da7f38f91e8b98e61cbf0df7612c3047f6289c
tuonybt <tonybt.tbt@gmail.com>
2023-10-26 09:14:01 +0100

    Reproduced 5-page example with disconnected graph

--- src/main copy 3.rs
+++ src/main copy 3.rs
@@ -1,32 +1,31 @@
 extern crate nalgebra as na;
 
-use na::{Vector5, Matrix5, Matrix, Const, ArrayStorage};
+use na::{Vector4, Matrix4, Matrix, Const, ArrayStorage};
 
 const THRESHOLD: f32 = 0.0000000000001;
-const BETA: f32 = 0.85;
+const BETA: f32 = 1.0;
 
 fn main() {
 
     // adjacency matrix
-    let arr = Matrix5::<f32>::from_columns(
-        &[Vector5::new(0.0, 1.0, 0.0, 0.0, 0.0),
-        Vector5::new(1.0, 0.0, 0.0, 0.0, 0.0),
-        Vector5::new(0.0, 0.0, 0.0, 1.0, 0.0),
-        Vector5::new(0.0, 0.0, 1.0, 0.0, 0.0),
-        Vector5::new(0.0, 0.0, 1.0, 1.0, 0.0)]
+    let arr = Matrix4::<f32>::from_columns(
+        &[Vector4::new(0.0, 1.0, 1.0, 1.0),
+        Vector4::new(0.0, 0.0, 1.0, 1.0),
+        Vector4::new(1.0, 0.0, 0.0, 0.0),
+        Vector4::new(1.0, 0.0, 1.0, 0.0)]
     );
     println!("Points awarded matrix: {:?}", arr);
 
     let s = arr.row_sum_tr();
     println!("Summation of columns: {:?}", s);
 
-    let m = Matrix5::<f32>::from_columns( 
+    let m = Matrix4::<f32>::from_columns( 
                 &arr.column_iter().zip(&s).map(|(c, &cs)| c / cs)
-                .collect::<Vec<Matrix<f32, Const<5>, Const<1>, ArrayStorage<f32, 5, 1>>>>()
+                .collect::<Vec<Matrix<f32, Const<4>, Const<1>, ArrayStorage<f32, 4, 1>>>>()
             );
     println!("Column stochastic probability matrix M: {:?}", m);
 
-    let r_new = Vector5::from_element(1.0 / m.ncols() as f32);
+    let r_new = Vector4::from_element(1.0 / m.ncols() as f32);
     println!("Initial rank vector: {:?}", r_new);
 
     let c = r_new.clone() * (1.0 - BETA);
@@ -35,7 +34,7 @@ fn main() {
     let mut r_prev = r_new.clone();
     println!();
 
-    for i in 1..11 {
+    for i in 1..1001 {
         print!("Iteration {}; ", i);
 
         let r_new = BETA * m * &r_prev + &c;
